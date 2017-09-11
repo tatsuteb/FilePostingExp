0b44bccec571ab8ea88faaaabad99156400cb651
kinjo <tatsuteb@gmail.com>
2017-09-12 01:08:04 +0900

    Add UserRegistrationViewModel

---
+++ FilePostingExp/ViewModel/UserRegistrationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace FilePostingExp.ViewModel
+{
+    public class UserRegistrationViewModel
+    {
+        public string UserName { get; set; }
+        public IFormFile ThumbnailFile { get; set; }
+    }
+}
